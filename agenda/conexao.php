42667593fcf5d44dd0604144e81eac331d7f3f21
Matheus Pepe <matheus.mello2601@gmail.com>
2025-11-24 23:55:48 +0000

    Implementa a conexão com o banco de dados MySQL

---
+++ agenda/conexao.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'agenda';
+
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+
+if ($conn->connect_error) {
+    die('Erro na conexão: ' . $conn->connect_error);
+}
+?>
